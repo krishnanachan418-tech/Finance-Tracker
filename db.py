b4ed4b1e337ee16420c623888af8f3accfada047
Priyansh Gautam <gautam.priyanash21@gmail.com>
2026-08-01 01:33:42 +0530

    Add DB_PORT support

--- db.py
+++ db.py
@@ -8,6 +8,7 @@ def get_db_connection():
     try:
         connection = mysql.connector.connect(
             host=os.getenv("DB_HOST", "localhost"),
+            port=int(os.getenv("DB_PORT", 3306)),
             user=os.getenv("DB_USER", "root"),
             password=os.getenv("DB_PASSWORD", ""),
             database=os.getenv("DB_NAME", "finance_tracker"),
